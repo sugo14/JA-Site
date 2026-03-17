3df4e0c4ff02cc2b3da83087b8363be783eac5d1
sugo14 <98863576+sugo14@users.noreply.github.com>
2026-03-17 14:24:29 -0600

    Added logo placeholder

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
 import { animate, utils, createDraggable, spring } from 'animejs';
 
-const [ $logo ] = utils.$('.logo.js');
-const [ $button ] = utils.$('button');
+const LOGO_ID = '.logo .outer';
+
+const [ $logo ] = utils.$(LOGO_ID);
 let rotations = 0;
 
-// Created a bounce animation loop
-animate('.logo.js', {
-  scale: [
-    { to: 1.25, ease: 'inOut(3)', duration: 200 },
-    { to: 1, ease: spring({ bounce: .7 }) }
-  ],
-  loop: true,
-  loopDelay: 250,
+// Rotate the outer logo
+animate(LOGO_ID, {
+	rotate: 360,
+	ease: "",
+	loop: true,
+	loopDelay: 0, 
+	duration: 7500
 });
 
 // Make the logo draggable around its center
-createDraggable('.logo.js', {
-  container: [0, 0, 0, 0],
-  releaseEase: spring({ bounce: .7 })
+createDraggable(LOGO_ID, {
+	container: [0, 0, 0, 0],
+	releaseEase: spring({ bounce: .7 })
 });
-
-// Animate logo rotation on click
-const rotateLogo = () => {
-  rotations++;
-  $button.innerText = `rotations: ${rotations}`;
-  animate($logo, {
-    rotate: rotations * 360,
-    ease: 'out(4)',
-    duration: 1500,
-  });
-}
-
-$button.addEventListener('click', rotateLogo);
