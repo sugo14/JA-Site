f279ef2b52e435bd7e27a5ca396198a5900d3538
sugo14 <98863576+sugo14@users.noreply.github.com>
2026-03-17 14:56:00 -0600

    Added navbar and other pages

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 import { animate, utils, createDraggable, spring } from 'animejs';
 
-const LOGO_ID = '.logo .outer';
+const LOGO_ID = '.logo';
+const LOGO_OUTER_ID = '.logo .outer';
 
 const [ $logo ] = utils.$(LOGO_ID);
 let rotations = 0;
 
+// Make the logo draggable around its center
+createDraggable(LOGO_ID, {
+	container: [0, 0, 0, 0],
+	releaseEase: spring({ bounce: .5 })
+});
+
 // Rotate the outer logo
-animate(LOGO_ID, {
+animate(LOGO_OUTER_ID, {
 	rotate: 360,
 	ease: "",
 	loop: true,
 	loopDelay: 0, 
 	duration: 7500
-});
-
-// Make the logo draggable around its center
-createDraggable(LOGO_ID, {
-	container: [0, 0, 0, 0],
-	releaseEase: spring({ bounce: .7 })
 });
